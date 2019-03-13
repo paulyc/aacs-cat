1dde90411b1ddd8e43801537cfea850076327990
Paul Ciarlo <paul.ciarlo@gmail.com>
2019-03-13 11:33:37 -0400

    decrypt unit keys, WIP decrypt blocks

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,65 @@ int find_vuk(IN const char *mkb_filename, IN uint8_t *vid, IN struct pk_entry *p
     return ret;
 }
 
+int decrypt_unit_key(IN uint8_t *vuk, IN const uint8_t *encrypted_unit_key, OUT uint8_t *decrypted_unit_key)
+{
+    gcry_cipher_hd_t gcry_h;
+    gcry_cipher_open(&gcry_h, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_ECB, 0);
+    gcry_cipher_setkey(gcry_h, vuk, 16);
+    gcry_cipher_decrypt(gcry_h, decrypted_unit_key, 16, encrypted_unit_key, 16);
+    gcry_cipher_close(gcry_h);
+    return 0;
+}
+
+/* 6144 bytes, 3 logical sectors */
+struct aacs_aligned_unit
+{
+    uint8_t seed[16];
+    uint8_t encrypted[6128];
+} __attribute__((packed));
+
+int decrypt_m2ts(IN const char *encrypted_filename, IN const char *decrypted_filename, IN uint8_t *unit_key)
+{
+    int ret = 0;
+
+    FILE *encrypted_m2ts = fopen(encrypted_filename, "rb");
+    FILE *decrypted_m2ts = fopen(decrypted_filename, "wb");
+    if (encrypted_m2ts == NULL) {
+        fprintf(stderr, "error opening encrypted m2ts %s\n", encrypted_filename);
+        return -1;
+    }
+    if (decrypted_m2ts == NULL) {
+        fprintf(stderr, "error opening decrypted m2ts %s\n", decrypted_filename);
+        fclose(encrypted_m2ts);
+        return -1;
+    }
+
+    struct aacs_aligned_unit unit;
+    while (!feof(encrypted_m2ts)) {
+        size_t rd = fread(&unit, sizeof(struct aacs_aligned_unit), 1, encrypted_m2ts);
+        if (rd == 0 && ferror(encrypted_m2ts)) {
+            fprintf(stderr, "error reading %s\n", encrypted_filename);
+            ret = ferror(encrypted_m2ts);
+            break;
+        }
+
+        // calculate block key from seed and unit key
+        uint8_t block_key[16];
+        gcry_cipher_hd_t gcry_h;
+        gcry_cipher_open(&gcry_h, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_ECB, 0);
+        gcry_cipher_setkey(gcry_h, unit_key, 16);
+        gcry_cipher_decrypt(gcry_h, block_key, 16, unit.seed, 16);
+        gcry_cipher_close(gcry_h);
+
+        for (int i = 0; i < 16; ++i) {
+        }
+    }
+
+    fclose(encrypted_m2ts);
+    fclose(decrypted_m2ts);
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     struct pk_entry pkl[1] = {{{0xAD,0x5E,0x54,0x6C,0x46,0xD7,0x2D,0xC0,0x83,0xAE,0xB5,0x68,0x69,0x24,0xE1,0xB3},NULL}};
@@ -111,9 +170,27 @@ int main(int argc, char **argv)
     uint8_t mk[16];
     uint8_t vuk[16];
 
-    if (find_vuk("./MKB_RO.inf", vid, pkl, sizeof(pkl), mk, vuk) == 0) {
-        printf("vuk is : ");
-        printhex(vuk, 16);
+    if (find_vuk("./MKB_RO.inf", vid, pkl, sizeof(pkl), mk, vuk) != 0) {
+        fprintf(stderr, "could not find vuk\n");
+        return -1;
+    }
+
+    printf("vuk is : ");
+    printhex(vuk, 16);
+    printf("\n");
+
+    const char *encrypted_unit_keys[] = {
+        "\x0f\x19\xc2\xfd\x1e\xb4\x56\xbd\x6d\x0c\x74\xae\xa8\xe7\xc2\x36",
+        "\xf3\x63\x25\xe2\x7a\xa5\xdc\xdf\xba\x15\x16\x83\x25\x74\x37\xe3",
+        "\x53\x12\xca\x39\x68\x7a\x3e\x72\x7b\xfa\x8c\x6b\x6b\x4d\x6e\xcd"
+    };
+    uint8_t decrypted_unit_keys[3][16];
+
+    for (int i = 0; i < 3; ++i) {
+        decrypt_unit_key(vuk, (const uint8_t*)encrypted_unit_keys[i], decrypted_unit_keys[i]);
+
+        printf("decrypted unit key %d : ", i);
+        printhex(decrypted_unit_keys[i], 16);
         printf("\n");
     }
 
